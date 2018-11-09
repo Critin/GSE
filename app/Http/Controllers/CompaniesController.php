38ddfe62ad3c311f3b24b83376fb937ef0b90169
Cristian Guerrero Medina <cristianguerreromedina@MacBook-Pro-de-Cristian.local>
2018-11-09 14:14:11 +0100

    Empresas v1
    Controlador, rutas, vistas y plantillas creadas.
    
    NOTA: No se ha probado su funcionamiento.

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -3,8 +3,100 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\GSE;
 
 class CompaniesController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $companies = Companies::orderBy('id','DESC')->paginate(3);
+        return view('Companies.index', compact('companies'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('Companies.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $this->validate($request,['name'=>'required', 'city'=>'required', 'pc'=>'required']);
+        
+        Companies::create($request->all());
+        return redirect()->route('companies.index')->with('success','¡Compañía registrada correctamente!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $companies = Companies::find($id);
+        return view('companies.show', compact('companies'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $company = Companies::find($id);
+        return view('companies.edit', compact('companies'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $this->validate($request,['name'=>'required', 'city'=>'required', 'pc'=>'required']);
+        
+        Companies::find($id)->update($request->all());
+        return redirect()->route('compaies.index')->with('success', '¡Compañía actualizada correctamente!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        Companies::find($id)->delete();
+        return redirect()->route('companies.index')->with('success', '¡Compañía eliminada correctamente!');
+    }
 }
