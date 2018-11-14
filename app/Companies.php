cd80e41b7a1e44c37b7e75aab26f33512aa198a8
Cristian Guerrero Medina <cristianguerreromedina@MacBook-Pro-de-Cristian.local>
2018-11-14 09:05:15 +0100

    Empresas v3.1
    La vista responde a la petición del controlador de crear una nueva empresa, pero no es capaz de editarla de momento.

--- app/Companies.php
+++ app/Companies.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Companies extends Model
 {
     protected $table = 'companies';
+
+    protected $fillable = [ 
+        'name', 'city', 'pc',
+    ];
 }
