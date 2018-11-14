be788c2ae5bd56ff6762207f87b9229443c1cbea
Cristian Guerrero Medina <cristianguerreromedina@MacBook-Pro-de-Cristian.local>
2018-11-14 10:11:33 +0100

    Personalización del Dashboard
    Traducción al español del formulario de la autenticación de usuario.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    ¡Ya has iniciado sesión!
                 </div>
             </div>
         </div>
