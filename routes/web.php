3bb3940e774d5318237b7072f863db169e8cc8d6
Cristian Guerrero Medina <cristianguerreromedina@MacBook-Pro-de-Cristian.local>
2018-11-14 08:58:36 +0100

    Empresas v3
    La vista carga lo que el controlador le pide, pero no guarda nuevos registros por un error que hasta ahora desconocemos.

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
 # Empresas
 Route::get('/companies', 'CompaniesController@index');
 Route::resource('companies', 'CompaniesController');
